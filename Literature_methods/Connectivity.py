921b908bd4ebac523b14b3d6838ef8d51d3ddea2
ShaharW <wineshtock@gmail.com>
2014-12-04 17:20:07 +0200

    changed 1nn to knn

--- Literature_methods/Connectivity.py
+++ Literature_methods/Connectivity.py
@@ -1,14 +1,17 @@
 '''
 Created on Nov 20, 2014
+@author: Shahar Weinstock
 
-@author: user
+Connectivity internal validity measure for clustering analysis as presented in Handl & Knowls 2005
+
+Input - Clustering labels, Distance matrix
 '''
-import numpy as np
-def Conn(labels,distanceMat):
+
+def Conn(labels,distanceMat,knn=10):
     not_match = 0.0
-    for index,label in enumerate(labels):
-        dist = distanceMat[index,:]
-        min_val = min(n for n in dist if n>0)
-        if label <> labels[list(dist).index(min_val)]:
-            not_match += 1
-    return (1-not_match/len(labels))
+    for index,label in enumerate(labels):                                      # run over all objects and labels
+        dist = sorted(distanceMat[index][:])                                   # sort the list of distances for the object
+        for i in range(1,knn+1):             # run on the knn of the object (skip the first distance from the object itself)
+            if label <> labels[list(distanceMat[index][:]).index(dist[i])]:    # if the objects are not from the same label
+                not_match += 1.0/knn                                           # add 1/knn to the count
+    return (1-not_match/len(labels))                                           # return the average connectivity of all objects
