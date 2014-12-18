14a92d87f6e155e32901a8b6fbd24888e4cc5705
ShaharW <wineshtock@gmail.com>
2014-12-18 09:42:26 +0200

    3rd order MM

--- My_methods/HMM.py
+++ My_methods/HMM.py
@@ -5,18 +5,18 @@ Created on Dec 7, 2014
 '''
 import numpy as np
 
-def HMM(data,order):
+def HMM(data,order=3):
     m = data.shape[0]
     n = data.shape[1]
     elements = int(max(np.unique(data))+1)
     models = []
     for i in xrange(m):
-        matrix = np.zeros((elements,elements))
+        print i
+        matrix = np.zeros((elements**order,elements))
         for j in xrange(n-order):
-            matrix[data[i,j],data[i,j+order]] += 1
-        for k in xrange(elements):
-            total = max([sum(matrix[k,:]),1])
-            matrix[k,:] =  matrix[k,:]/total
-        models.append(matrix)    
-            
-    np.save('C:\Users\user\workspace'+str(order)+'//HMM_Sunday', models)  # @UndefinedVariable
+            matrix[data[i,j]*elements**2+data[i,j+1]*elements+data[i,j+2],data[i,j+order]] += 1
+        for k in xrange(elements**order):
+            total = sum(matrix[k,:]) + 1
+            matrix[k,:] =  (matrix[k,:] + 1.0/elements)/total
+        models.append(matrix) 
+    np.save('C:\Users\user\workspace\\3HMM_Sunday', models)  # @UndefinedVariable
