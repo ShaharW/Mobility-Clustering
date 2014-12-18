3eccba1dbbed808d8090ac4de0bc2b140e9a4f58
ShaharW <wineshtock@gmail.com>
2014-12-18 16:48:29 +0200

    return distMat instead of saving

--- Literature_methods/LCSS_distMat.py
+++ Literature_methods/LCSS_distMat.py
@@ -1,27 +1,22 @@
 '''
 Created on Sep 9, 2014
 
-@author: user
+@author: Shahar Weinstock
 '''
-
-import sys
+import os, sys, inspect, time  # @UnusedImport
 sys.path.insert(0, 'C:\Users\user\Anaconda\Lib\site-packages')
-import numpy, time, LCSS
+import numpy as np
+import LCSS
 
-start = time.time()
-path ='C:\Users\user\workspace'
-data = numpy.loadtxt(path+"\Sunday-clean.csv",delimiter = ',')
-length, width = data.shape
-width *= 1.0
-dist = numpy.zeros((length,length))
-
-for i in xrange(length):
-    print i
-    for j in xrange(i+1):
-        dist[i,j] = 1-LCSS.lcs(data[i,:],data[j,:])/width
-        dist[j,i] = dist[i,j]
-
-numpy.savetxt('LCSS_distMat-Sunday.txt', dist)
-
-end = time.time()
-print "calculation time is %s seconds " %str(int(end-start))
+def LCSS_distMat(data):
+    start = time.time()
+    length, width = data.shape
+    width *= 1.0
+    dist = np.zeros((length,length))
+    for i in xrange(length):
+        for j in xrange(i+1):
+            dist[i,j] = 1-LCSS.lcs(data[i,:],data[j,:])/width
+            dist[j,i] = dist[i,j]
+    end = time.time()
+    print "calculation time is %s seconds " %str(int(end-start))
+    return dist
