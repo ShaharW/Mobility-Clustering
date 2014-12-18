52286915b88171469c863904ca161dc166676aa9
ShaharW <wineshtock@gmail.com>
2014-12-18 16:50:12 +0200

    return models instead of saving

--- My_methods/HMM.py
+++ My_methods/HMM.py
@@ -5,7 +5,7 @@ Created on Dec 7, 2014
 '''
 import numpy as np
 
-def HMM(data,order=3):
+def HMM(data,order=1):
     m = data.shape[0]
     n = data.shape[1]
     elements = int(max(np.unique(data))+1)
@@ -14,9 +14,10 @@ def HMM(data,order=3):
         print i
         matrix = np.zeros((elements**order,elements))
         for j in xrange(n-order):
-            matrix[data[i,j]*elements**2+data[i,j+1]*elements+data[i,j+2],data[i,j+order]] += 1
+            row = sum(data[i,k]*elements**(j+order-k-1) for k in range(j,j+order))
+            matrix[row,data[i,j+order]] += 1
         for k in xrange(elements**order):
             total = sum(matrix[k,:]) + 1
             matrix[k,:] =  (matrix[k,:] + 1.0/elements)/total
         models.append(matrix) 
-    np.save('C:\Users\user\workspace\\3HMM_Sunday', models)  # @UndefinedVariable
+    return models
