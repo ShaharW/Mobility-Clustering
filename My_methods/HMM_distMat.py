d3723618279313c9430870cf9b11c3a82d11b9c8
ShaharW <wineshtock@gmail.com>
2014-12-18 09:43:26 +0200

    KL distance with apriory probs

--- My_methods/HMM_distMat.py
+++ My_methods/HMM_distMat.py
@@ -11,9 +11,7 @@ import numpy as np
 def HMM_distMat(models):
     distMat = np.zeros((models.shape[0],models.shape[0]))
     for index_a,mat_a in enumerate(models):
-        print index_a
-        print mat_a
         for index_b,mat_b in enumerate(models):
-            [(val = 0.01) for val in mat_b[index] for index, t in enumerate(mat_b) if val == 0]
-            distMat[index_a,index_b] = stats.entropy(mat_a,mat_b)
-    return distMat
+            distMat[index_a,index_b] = sum(stats.entropy(mat_a[i,:],mat_b[i,:]) for i in range(mat_a.shape[0]))
+    distMat = distMat/distMat.max()
+    np.savetxt('C:\Users\user\workspace\Results\\3HMM_distMat.txt', distMat)
