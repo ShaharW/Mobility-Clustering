23d894f43b400ae926508aaa39a4a03293b74d58
shaharw <wineshtock@gmail.com>
2015-03-13 16:20:40 +0200

    faster implementation

--- Internal_validity_indices/Dunn_index.py
+++ Internal_validity_indices/Dunn_index.py
@@ -3,54 +3,66 @@ Created on Oct 30, 2014
 
 @author: Shahar Weinstock
 '''
+import os, sys, inspect, time  # @UnusedImport
+sys.path.insert(0, 'C:\Users\user\Anaconda\Lib\site-packages')
+import numpy as np
 
-def cluster_size(cluster,labels):
-    size = 0
-    for l in labels:
-        if l == cluster:
-            size += 1
-    return size
+def Dunn(labels,distanceMat):
+    k = int(max(labels)+1)
+    clusters = [cluster(count,labels,distanceMat) for count in xrange(k)]
+    return min_inter_distance(k,labels,distanceMat,clusters)/max_intra_distance(k,labels,distanceMat,clusters)
 
-def intra_cluster_distance(cluster, labels, distanceMat):
-    dist = 0.0
-    size = cluster_size(cluster,labels)
-    for i in range(len(labels)):
-        if labels[i] == cluster:
-            for j in range(i+1,len(labels)):
-                if labels[j] == cluster:
-                    dist += distanceMat[i][j]
-    if dist == 0:
-        print ("The cluster has only one element")
-        return 0
-    return dist/((size)*(size-1)*0.5)
+def max_intra_distance(k,labels,distanceMat,clusters):
+    intra_distances = []
+    for i in range(k):
+            intra_distances.append(clusters[i].max_intra)
+    return max(intra_distances)
 
 def inter_cluster_distance(cluster1, cluster2, labels, distanceMat):
     dist = 0.0
-    for i in range(len(labels)):
-        if labels[i] == cluster1:
-            for j in range(len(labels)):
-                if labels[j] == cluster2:
-                    dist += distanceMat[i][j]
-    size1 = cluster_size(cluster1,labels)
-    size2 = cluster_size(cluster2,labels)
-    if size1 == 0 or size2 == 0:
+    if cluster1.size == 0 or cluster2.size == 0:
         return float('Inf')
-    return dist/(size1*size2)   
+    for i in cluster1.instances:
+        for j in cluster2.instances:
+            dist += distanceMat[i][j]
+    return dist/(cluster1.size*cluster2.size)   
 
-def min_inter_distance(k,labels,distanceMat):
+def min_inter_distance(k,labels,distanceMat,clusters):
     inter_distances = []
     for i in range(k):
-            for j in range(i+1,k):
-                inter_distances.append(inter_cluster_distance(i, j, labels, distanceMat))
+        for j in range(i+1,k):
+            inter_distances.append(inter_cluster_distance(clusters[i], clusters[j], labels, distanceMat))
     return min(inter_distances)
 
-def max_intra_distance(k,labels,distanceMat):
-    intra_distances = []
-    for i in range(k):
-            intra_distances.append(intra_cluster_distance(i, labels, distanceMat))
-    return max(intra_distances)
-
-def Dunn(labels,distanceMat):
-    return min_inter_distance(len(set(labels)),labels,distanceMat)/max_intra_distance(len(set(labels)),labels,distanceMat)
-
+''' Cluster class '''        
+class cluster:
+    def __init__(self,number,labels,distanceMat):
+        self.number = number
+        self.size = self.get_cluster_size(labels)
+        self.instances = self.get_cluster_data(labels)
+        self.max_intra = self.intra_cluster_distance(labels,distanceMat)
         
+    def get_cluster_size(self,labels):
+        k = int(max(labels)+1)
+        return np.histogram(labels,bins=np.arange(k+1))[0][self.number]
+    
+    def get_cluster_data(self,labels):
+        if self.size == 0:
+            return None
+        m = len(labels)
+        j = 0
+        cluster_data = np.zeros((self.size))
+        for i in range(m):
+            if labels[i] == self.number:
+                cluster_data[j] = i
+                j += 1
+        return cluster_data
+
+    def intra_cluster_distance(self, labels, distanceMat):
+        dist = 0.0
+        if self.size == 0:
+            return 0
+        for i in range(self.size):
+            for j in range(i+1,self.size):
+                dist += distanceMat[self.instances[i]][self.instances[j]]
+        return dist/((self.size)*(self.size-1)*0.5)
