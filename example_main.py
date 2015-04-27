06d15fc1953efb744b3c70b0cf412b17c364cadb
shaharw <wineshtock@gmail.com>
2015-04-27 22:49:16 +0300

    changes

--- example_main.py
+++ example_main.py
@@ -7,6 +7,7 @@ import os, sys, inspect, time  # @UnusedImport
 sys.path.insert(0, 'C:\Users\user\Anaconda\Lib\site-packages')
 import numpy as np
 from sklearn import metrics
+from Clustering_methods import Kmedoids
 from My_methods import Markov, Markov_JS_distMat
 from Literature_methods import LCSS_distMat
 
@@ -14,8 +15,8 @@ from Literature_methods import LCSS_distMat
 # name = '\JS_DBSCAN'
 # dist_name = '\\3MM_JS_distMat.txt'
 models_name = '\same_1MM.npy'
-data_name = '\data2.csv'
-path = 'C:\Users\user\workspace\Results\New example' 
+data_name = '\example data-indel.csv'
+path = 'C:\Users\user\workspace\Results\Example' 
 # if not os.path.exists(path+):os.makedirs(path+name)
  
 '''***************Load data***************'''
@@ -24,20 +25,17 @@ start = time.time()
 print "loading data"
 # dist = np.loadtxt(path+dist_name,delimiter = ' ')  # @UndefinedVariable
 data = np.loadtxt(path+data_name,delimiter = ',')  # @UndefinedVariable
-models = np.load(path+models_name)  # @UndefinedVariable
-# Aff = 1-dist
+# models = np.load(path+models_name)  # @UndefinedVariable
+
 end = time.time()
 print "load time is %s seconds " %str(int(end-start))
 
-#===============================================================================
-# models = Markov.MM(data, 1)
-# np.save(path+'\different_1MM', models)  # @UndefinedVariable
-#===============================================================================
+analysis_name = '\\indel'
 
+####################################################################
+models = np.asarray(Markov.MM(data, 1))
 dist = Markov_JS_distMat.MM_distMat(models)
-np.savetxt(path+'\\same_JS_distMat.csv', dist, delimiter=",")  # @UndefinedVariable
-
-#===============================================================================
-# dist = LCSS_distMat.LCSS_distMat(data)
-# np.savetxt(path+'\\same_LCSS_distMat.csv', dist, delimiter=",")  # @UndefinedVariable
-#===============================================================================
+np.savetxt(path+analysis_name+'_Markov.csv', dist, fmt='%.2f', delimiter=",")  # @UndefinedVariable
+####################################################################
+dist = LCSS_distMat.LCSS_distMat(data)
+np.savetxt(path+analysis_name+'_LCSS.csv', dist, fmt='%.2f', delimiter=",")  # @UndefinedVariable
