fd43d8dd9a200a7c69bffa24f226b12a95648be2
shaharw <wineshtock@gmail.com>
2015-04-27 22:47:16 +0300

    added prints

--- Literature_methods/Euclidean_distMat.py
+++ Literature_methods/Euclidean_distMat.py
@@ -9,16 +9,16 @@ import numpy as np
 import scipy
             
 def distMat(data):
+    print "Calculating Euclidean_distMat"
     start = time.time()
     length, width = data.shape
     width *= 1.0
     dist = np.zeros((length,length))
     for i in xrange(length):
-        print "instance - " + str(i)
         for j in xrange(i+1):
             dist[i,j] = scipy.spatial.distance.euclidean(data[i,:],data[j,:])
             dist[j,i] = dist[i,j]
     dist = dist/dist.max() # normalize
     end = time.time()
-    print "calculation time is %s seconds " %str(int(end-start))
+    print "Euclidean_distMat calculation time is %s minutes " %str(int(end-start)/60)
     return dist
